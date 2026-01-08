9948343a649fa967afd940cb6f5f05da6def5a7c
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-01-08 12:36:54 +0100

    update server.js and install axios

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const connect = require('./connect')
 const express = require('express')
 const cors = require('cors')
+const tasks = require('./routes/tasks')
 
 const app = express()
 const PORT = 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(tasks)
 
 app.listen(PORT, () => {
   connect.connectToServer()
