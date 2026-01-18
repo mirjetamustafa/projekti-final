615976584449dc82ce33f91bb791a1bec13c8f39
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-01-18 16:22:32 +0100

    update components

--- taskManager/src/components/shared/Button/Button.tsx
+++ taskManager/src/components/shared/Button/Button.tsx
@@ -1,7 +1,7 @@
 type ButtonProps = {
   children: React.ReactNode
   type?: 'button' | 'submit'
-  variant?: 'active' | 'icon'
+  variant?: 'default' | 'active' | 'icon'
   className?: string
 }
 
@@ -9,10 +9,11 @@ const Button = ({
   children,
   onClick,
   type = 'button',
-  variant = 'active',
+  variant = 'default',
   className = '',
 }: ButtonProps) => {
   const variants: Record<string, string> = {
+    default: 'w-full text-xs font-bold text-gray-700 px-3 py-2 rounded-md ',
     active: 'w-full text-xs font-bold text-gray-700 px-3 py-2 rounded-md',
     icon: 'w-auto p-2 text-white bg-blue-600 hover:bg-blue-700 flex items-center justify-center',
   }
